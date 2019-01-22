131231ef15903d3470d450445487ebbf7abf863d
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2019-01-22 23:04:43 +0330

    Add aboutus button

--- WebApp/src/js/index.js
+++ WebApp/src/js/index.js
@@ -20,72 +20,63 @@ uploadPlaceDropZone.on('thumbnail', function(file) {
     // Disable dropzone on upload place so that clicking wont bring browse window
     uploadPlaceDropZone.disable();
 
-    preview = document.querySelector('#preview')
-    preview.src = file.dataURL;
-
     // Get image preview canvas context
     ctx = imageCanvas.getContext('2d');
     // Create an image and add selected images data url
-    // var imageObj = new Image();
-    // imageObj.src = file.dataURL;
+    var imageObj = new Image();
+    imageObj.src = file.dataURL;
     // After data url is added to image, draw it on convas
-    // imageObj.onload = function() {
-    //     // calculate a Suitable size for resizing image
-    //     // (Image might be too big)
-    //     aspect = file.height / file.width;
-    //     width = 400;
-    //     height = width * aspect;
-
-    //     console.log(imageObj.width);
-    //     console.log(imageObj.height);
+    imageObj.onload = function() {
+        // calculate a Suitable size for resizing image
+        // (Image might be too big)
+        aspect =  file.width / file.height;
+        height = 600;
+        width = height * aspect;
         
+        Object.assign(uploadPlace.style, {
+            width: `${width}px`,
+            height: `${height}px`
+        });
+
+        console.log(width, height);
+        
+        
+        imageCanvas.style.display = 'block';
+        imageCanvas.width = width;
+        imageCanvas.height = height;
 
     
-    //     ctx.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height, 0, 0, width, height);
-    // };
-    Object.assign(uploadPlace.style, {
-        width: `${file.width}px`,
-        height: `${file.height}px`
-    });
-    imageCanvas.style.display = 'block';
-    imageCanvas.width = file.width;
-    imageCanvas.height = file.height;
-    
+        ctx.drawImage(imageObj, 0, 0, file.width, file.height, 0, 0, width, height);
 
-    setTimeout(() => {
-        console.log(imageCanvas.width);
-        console.log(imageCanvas.height);
-        ctx.drawImage(preview, 0, 0);
-    }, 2000);
-
-
-    
-
-
-    // preview = document.querySelector('#preview');
-    // preview.src = file.dataURL;
-    // let src = cv.imread(preview);
-    // let dst = src.clone()
-    // // let dst = cv.Mat.zeros(src.cols, src.rows, cv.CV_8UC3);
-    // cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    // cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
-    // let contours = new cv.MatVector();
-    // let hierarchy = new cv.Mat();
-    // // You can try more different parameters
-    // cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-    // // draw contours with random Scalar
-    // let color = new cv.Scalar(255, 0, 0, 255);
-    // for (let i = 0; i < contours.size(); ++i) {
-    //     area = cv.contourArea(contours.get(i), false);
-    //     console.log(area)
-    //     if (area <= 50 && area >= 30)
-    //         cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-    // }
-    // cv.imshow('imageCanvas', dst);
-    // src.delete(); dst.delete(); contours.delete(); hierarchy.delete();
-    
+        setTimeout(() => {
+            drawContoures(imageCanvas);
+        }, 500);
+    };
 });
 
 function onOpenCvReady() {
     console.log('onOpenCvReady')
+}
+
+function drawContoures(canvas) {
+    let src = cv.imread(canvas);
+    let dst = src.clone()
+    // let dst = cv.Mat.zeros(src.cols, src.rows, cv.CV_8UC3);
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    // You can try more different parameters
+    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+    // draw contours with random Scalar
+    let color = new cv.Scalar(255, 0, 0, 255);
+    for (let i = 0; i < contours.size(); ++i) {
+        area = cv.contourArea(contours.get(i), false);
+        if (area <= 130 && area >= 100){
+            console.log(area, cv.boundingRect(contours.get(i)))
+            cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+        }
+    }
+    cv.imshow('imageCanvas', dst);
+    src.delete(); dst.delete(); contours.delete(); hierarchy.delete();
 }
